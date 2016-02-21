e85ddb40cc1f90e956961fad94958c9ab70aa232
Stepan Lusnikov <endenwer@gmail.com>
2016-02-21 19:40:03 +0300

    Change gem settings in railtie

--- lib/npm/rails/railtie.rb
+++ lib/npm/rails/railtie.rb
@@ -5,17 +5,8 @@ module Npm
   module Rails
     class Railtie < ::Rails::Railtie
       config.npm = ActiveSupport::OrderedOptions.new
-      config.npm.directory = "npm"
-      config.npm.packages_file = "npm_packages"
-
-      initializer "npm_rails.setup_vendor" do |app|
-        browserify_output_directory = Rails.root.join("#{ app.config.npm.directory }/generated")
-        if Rails.env.production?
-          app.config.assets.append_path("#{ browserify_output_directory  }/prod-bundle")
-        else
-          app.config.assets.append_path("#{ browserify_output_direcotry  }/dev-bundle")
-        end
-      end
+      config.npm.package_file = "npm_packages"
+      config.npm.output_file = "vendor/assets/javascripts/npm-dependencies.js"
 
       rake_tasks do
         load "tasks/npm.rake"
